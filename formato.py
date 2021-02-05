69934f8534c9b40521feeb4ceedf73ea985fa7bd
Evelyn19-Ulco <76453340+Evelyn19-Ulco@users.noreply.github.com>
2021-02-05 11:44:50 -0500

    Add files via upload

---
+++ formato.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 15:29:20 2021
+
+@author: DELL
+"""
+
+pi=22/7
+print(pi)
+print('{:.5f}'.format(pi))
+print('{:.4f}'.format(pi))
+print('{:.50f}'.format(pi))
+print('{:.14f}'.format(pi))
+
